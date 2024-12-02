dde89b3d0b5bd02b4e70e3eacce17b625be8e6bd
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 15:18:40 +0530

    Add files via upload

---
+++ nc.java
@@ -0,0 +1,10 @@
+public class nc {
+    public static void main(String args[]){
+        char c='a';
+        int b= c;
+        b+=1;
+        char v=(char)(b);
+        System.out.println(v);
+    }
+
+}
