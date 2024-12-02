6bfa641c5dee4fbf82c54a3f33e872e1bc063f92
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 14:39:09 +0530

    Add files via upload

---
+++ dt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class dt {
+    public static void main(String args[]){
+        int a=53;
+        char b='A';
+        float c=1.25f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
