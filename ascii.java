e5555ec793bebb11e03d578a8585f34c8a134885
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 15:02:59 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ascii {
+    public static void main(String args[]){
+        //converting of ascii valueto char
+        int a=50;
+        char b=(char) (a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
