e18ac102c46453270b4ea8d911ed76f171ee11a5
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 16:23:10 +0530

    Add files via upload

---
+++ sr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sr {
+    public static void main(String args[]){
+        Scanner R=new Scanner(System.in);
+        String c=R.next();
+        String a=R.next();
+        String b= String.format("the name of the person is %s and age %s",c,a);
+        System.out.println(b);
+    }
+}
