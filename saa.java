fdff1dfb8b5ed3e1f4cf837f9b9739b61bdb0315
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 16:08:21 +0530

    Add files via upload

---
+++ saa.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class saa {
+    public static void main(String args[]){
+        Scanner R=new Scanner(System.in);
+        String c=R.next();
+        String b= String.format("congratulations %s you won 25 lakhs",c);
+        System.out.println(b);
+}
+}
