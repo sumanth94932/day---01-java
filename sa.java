83dc240bd3e6acdc6eb8ee7f70a4a1d2b3fd09df
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-02 15:57:43 +0530

    Add files via upload

---
+++ sa.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class sa {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(" congratulatons " +a+ " u won 10 lakhs ");
+    }
+}
